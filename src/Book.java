85487000698837692c1184e5a025c774435b3c09
DmitryRubtsov53 <2578390@mail.ru>
2022-09-29 19:12:00 +0500

    Решено ДЗ к Уроку "Объекты и Классы".

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,23 @@
 public class Book {
+    private final String bookName;
+    int yearPublishing;
+    Autor autor;
+//Если ввести - Autor autor = new Autor(String firstName,  String lastName), то "return autor" тоже будет подчёркнут
+
+    public  String getBookName() {
+        return this.bookName;
+    }
+    public Autor getAutor() { return this.autor; }
+    public int getYearPublishing() { return yearPublishing; }
+    public void setYearPublishing(int yearPublishing) {
+        this.yearPublishing = yearPublishing;
+    }
+
+    public Book (String bookName, Autor autor, int yearPublishing) {
+        this.bookName = bookName;
+        this.autor = autor;
+        this.yearPublishing = yearPublishing;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Домашнее задание Урока Объекты и Классы *************************************************");
+        System.out.println();
+        // 1-я книга
+        Autor autor1 = new Autor("Лев", "Толстой");
+        Book book1 = new Book("Война и мир", autor1 , 1970);
+        // 2-я книга
+        Autor autor2 = new Autor("Джек", "Лондон");
+        Book book2 = new Book("Мартин Иден", autor2 , 1985);
+        // Выводим на консоль 1-ю книгу
+
+        System.out.print("Книга: " + book1.getBookName() + ". ");
+
+        System.out.print("Автор: " + book1.getAutor().getFirstName() + " " + book1.getAutor().getLastName() + ". ");
+        System.out.print("Год издания: " + book1.getYearPublishing());
+        // Выводим на консоль 2-ю книгу
+        System.out.println();
+
+        System.out.print("Книга: " + book2.getBookName() + ". ");
+        System.out.print("Автор: " + book2.getAutor().getFirstName() + " " + book2.getAutor().getLastName() + ". ");
+        System.out.print("Год издания: " + book2.getYearPublishing());
+
+        System.out.println();
+        book1.setYearPublishing(2000);
+        System.out.print("Новое издание книги " + book1.getBookName() + ", год " + book1.getYearPublishing());
 
     }
 }
